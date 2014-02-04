36c3ba3d4c1b3491b2287038f5f2f69d533fdf85
João Pinho <jpe.pinho@gmail.com>
2014-02-04 22:15:56 +0000

    Initial Commit

---
+++ MyCodingSolutions/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyCodingSolutions
+//
+//  Created by JP on 04/02/14.
+//  Copyright (c) 2014 CodeBox. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
