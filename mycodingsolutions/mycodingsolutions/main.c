74f86274bd93835453d6c98c29ac60bed55666bc
João Pinho <jpe.pinho@gmail.com>
2014-02-05 02:06:45 +0000

    cleaning up

---
+++ mycodingsolutions/mycodingsolutions/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mycodingsolutions
+//
+//  Created by JP on 05/02/14.
+//  Copyright (c) 2014 CodeBox. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
