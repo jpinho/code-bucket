ef3a5bb388c864f4b6149e0a2356f964c9624155
João Pinho <jpe.pinho@gmail.com>
2014-02-05 18:53:48 +0000

    add new problems solved

---
+++ src/deletenode_singlell.c
@@ -0,0 +1,9 @@
+//
+//  deletenodeinsll.c
+//  mycodingsolutions
+//
+//  Created by JP on 05/02/14.
+//  Copyright (c) 2014 CodeBox. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ src/dlinkedlist.h
@@ -0,0 +1,9 @@
+//
+//  dlinkedlist.h
+//  mycodingsolutions
+//
+//  Created by JP on 05/02/14.
+//  Copyright (c) 2014 CodeBox. All rights reserved.
+//
+
+#include <stdio.h>
