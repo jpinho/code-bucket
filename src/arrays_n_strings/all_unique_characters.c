c34bcb1ae61eabddfca5e14076456d6908f0bd95
João Pinho <jpe.pinho@gmail.com>
2014-02-06 01:39:49 +0000

    added folders by problem category

---
+++ src/arrays_n_strings/all_unique_characters.c
@@ -0,0 +1,69 @@
+/**
+ *  Question:
+ *  - Implement an algorithm to determine if a string has all unique characters.
+ *
+ *  Assumptios:
+ *  - Assuming the text given contains only caracters between [a-z] (ANSI, 97-122).
+ *
+ *  @author João Pinho
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int checkAllUniqueChars(char[]);
+
+int main_1(int argc, char* argv[])
+{
+    char text[60];
+    printf("\nthis program detects whether all caracters in a string are all unique.");
+    printf("\nlet's start, write something:");
+    
+    if(fgets(text, sizeof(text), stdin) == NULL){
+        perror("No input specified.\n");
+        exit(1);
+    }
+	
+    if(!checkAllUniqueChars(text)){
+        printf("\nyou have repeated characters in your text\n");
+    }
+    else printf("\nthe caracteres in you text are all unique\n");
+    
+    return 0;
+}
+
+/** 
+ * Checks unique chars in @text, assuming @text contains characters only between [a-z].
+ *
+ * @param text a string containing the letters to be checked for unique caracters.
+ * @return 0 for false and 1 for true.
+ */
+int checkAllUniqueChars(char text[])
+{
+    int checker = 0;
+    char c;
+    int i=0;
+    
+    // if the number of chars in text is greater that the number of caracters being checked
+    // for uniqueness, then is impossible to not have a duplicated caracter.
+    if(strlen(text) > ('z'-'a'))
+        return 0;
+    
+    while((c = text[i++]) != 0)
+    {
+        // if [c] is 'a' then 'a' - 'a' (=) 97-97 = 0, so mask is 0000 ... 0001.
+        int letterMask = 1 << (c - 'a');
+        
+        // if equal, the bit of letter [c] is already on, so, with one more it
+        // means letter [c] is duplicated.
+        if(checker == (checker | letterMask))
+            return 0;
+        
+        // activates the letter bit in the checker mask.
+        checker |= letterMask;
+    }
+    
+    return 1;
+}
---
+++ src/linkedlists/deletenode_singlell.c
@@ -0,0 +1,56 @@
+/**
+ *  Question: 
+ *  - Implement an algorithm to delete a node in the middle of a singly linked list, 
+ *    given only access to that node.
+ *    
+ *    EXAMPLE
+ *    Input: the node c from the linked list a->b->c->d->e
+ *    
+ *    Result: nothing is returned, but the new linked list looks like a->b->d->e
+ *
+ *  @author João Pinho
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dlinkedlist.h"
+
+void deleteNode(ll_node*);
+
+/**
+ * My idea for this one is: if you only see whats in front of you, and you dont matter no more,
+ * then give (recursively) your place to the nodes standing in front of you, and pass the message
+ * to then, saying the should do the same. In the end, the last element, should clean up the extra
+ * space lef calling free upon is old place.
+ *
+ * Note: I'm using Double Linked List without using the pointer prev, so it's like using SingleLL.
+ */
+int main(int argc, char* argv[]){
+    ll_node* list = ll_create();
+
+    ll_setValue(list, 'a');
+    ll_appendNode(list, 'b');
+    ll_appendNode(list, 'c');
+    ll_appendNode(list, 'd');
+    ll_appendNode(list, 'e');
+    ll_node* nodeC = ll_appendNode(list, 'f');
+    ll_printc(list);
+ 
+    deleteNode(nodeC);
+    ll_printc(list);
+    
+    return 0;
+}
+
+void deleteNode(ll_node* nd){
+    if(nd==NULL) return;
+    
+    if(nd->next == NULL){
+        nd->data = DUMMY_NVALUE;
+        return;
+    }
+    
+    ll_setValue(nd, ll_getValue(nd->next));
+    nd->next = nd->next->next;
+    free(nd->next);
+}
