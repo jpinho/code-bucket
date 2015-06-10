a78f2fed8370d5c8cb759766bbf28ff66b9adbc4
João Pinho <jpe.pinho@gmail.com>
2015-06-10 15:56:34 +0100

    [ADD] New solution (corp interview)

---
+++ corp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorpInterview
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var nums = new int[]{ 1, 2, 3, 4, 5, 6 };
+			var letters = new int[]{ 'a', 'b', 'c', 'd', 'e', 'f' };
+			var result = ClapIt (nums, letters);
+
+			Console.WriteLine ("in: [1, 2, 3, 4, 5, 6] [a, b, c, d, e, f]");
+			Console.Write ("out: "); 
+			Print (result);
+		}
+
+		public static int[][] ClapIt(int[] nums, int[] letters){
+			var lstOdd = new List<int> (nums.Length);
+			var lstEvenRvs = new List<int> (nums.Length);
+
+			for (int odd = 0, even = nums.Length-1; odd < nums.Length || even > 0; odd += 2, even -= 2) {
+				lstOdd.Add (nums[odd]);
+				lstOdd.Add (letters [odd]);
+				lstEvenRvs.Add (nums[even]);
+				lstEvenRvs.Add (letters [even]);
+			}
+
+			return new int[2][]{ lstOdd.ToArray (), lstEvenRvs.ToArray () };
+		}
+
+		public static void Print(int[][] result){
+			for (int i = 0; i < result.Length; i++) {
+				Console.Write ("[ ");
+				for (int j = 0; j < result [i].Length; j++) {
+					if(Char.IsLetter((char)result[i][j]))
+						Console.Write ((char)result[i][j] + " ");
+					else
+						Console.Write (result[i][j] + " ");
+				}
+				Console.Write ("] ");
+			}
+		}
+	}
+}
---
+++ corp/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("CorpInterview")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("pinho.icodebox.net")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.*")]
